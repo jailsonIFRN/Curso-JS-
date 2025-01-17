aeb882e4183ceba76a9adaa44e2651fa3733ad31
notebook_pessoal <professorjailsoninformatica@gmail.com>
2025-01-17 09:39:09 -0300

    ESTRUTURA DE PROGRAMAÇÃO

--- 01_INTRO/js/scripts.js
+++ 01_INTRO/js/scripts.js
@@ -1,2 +1,4 @@
 console.log("Testando JS 2");
 console.log("Olá, Mundo");
+let idade = 25;
+console.log("Idade:", idade);
--- 02_OPERADORES/js/scripts.js
+++ 02_OPERADORES/js/scripts.js
@@ -3,97 +3,97 @@ console.log(typeof 2);
 console.log(typeof 2.2);
 console.log(typeof -4);
 
-// 2 - Ops. aritméticas
+// // 2 - Ops. aritméticas
 
-console.log(2 + 4);
-console.log(10 - 5);
-console.log(5 * 4);
-console.log(10 / 2);
+// console.log(2 + 4);
+// console.log(10 - 5);
+// console.log(5 * 4);
+// console.log(10 / 2);
 
-// 3 - Special Numbers
-console.log(typeof Infinity);
+// // 3 - Special Numbers
+// console.log(typeof Infinity);
 
-console.log(12 * "asd");
+// console.log(12 * "asd");
 
-console.log(typeof NaN);
+// console.log(typeof NaN);
 
-// 4 - Strings
-console.log("Um texto");
-console.log(`Mais um texto`);
+// // 4 - Strings
+// console.log("Um texto");
+// console.log(`Mais um texto`);
 
-console.log(typeof "Opa");
+// console.log(typeof "Opa");
 
-// 5 - Símbolos especiais em string
-console.log("Testando \n quebra de linha");
+// // 5 - Símbolos especiais em string
+// console.log("Testando \n quebra de linha");
 
-console.log("Espaçamento \t de tab");
+// console.log("Espaçamento \t de tab");
 
-// 6 - Concatenação
-console.log("Oi, " + " tudo " + "bem?");
+// // 6 - Concatenação
+// console.log("Oi, " + " tudo " + "bem?");
 
-console.log(`Testando ` + `Concatenação`);
+// console.log(`Testando ` + `Concatenação`);
 
-// 7 - Interpolação
-console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+// // 7 - Interpolação
+// console.log(`A soma de 2 + 2 é: ${2 + 2}`);
 
-console.log(`Podemos executar qualquer coisa ${console.log("teste")}`);
+// console.log(`Podemos executar qualquer coisa ${console.log("teste")}`);
 
-// 8 - Booleans
-console.log(5 > 2);
+// // 8 - Booleans
+// console.log(5 > 2);
 
-console.log(30 > 100);
+// console.log(30 > 100);
 
-console.log(typeof true);
+// console.log(typeof true);
 
-// 9 - Comparações
-console.log(5 <= 5);
+// // 9 - Comparações
+// console.log(5 <= 5);
 
-console.log(5 < 5);
+// console.log(5 < 5);
 
-console.log(10 == 10);
+// console.log(10 == 10);
 
-console.log(10 == 9);
+// console.log(10 == 9);
 
-console.log(10 != 9);
+// console.log(10 != 9);
 
-// 10 - Idêntico
-console.log(9 == "9");
+// // 10 - Idêntico
+// console.log(9 == "9");
 
-console.log(9 === "9");
+// console.log(9 === "9");
 
-console.log(9 != "9");
+// console.log(9 != "9");
 
-console.log(9 !== "9");
+// console.log(9 !== "9");
 
-// 11 - op logico na prática
-console.log(true && true);
+// // 11 - op logico na prática
+// console.log(true && true);
 
-console.log(true && false);
+// console.log(true && false);
 
-console.log(5 > 2 && 2 < 10);
+// console.log(5 > 2 && 2 < 10);
 
-console.log(5 > 2 && "Matheus" === 1);
+// console.log(5 > 2 && "Matheus" === 1);
 
-console.log(10 > 2 || 5 > 100);
+// console.log(10 > 2 || 5 > 100);
 
-console.log(!50 > 10);
+// console.log(!50 > 10);
 
-// 12 - empty values
-console.log(typeof null, typeof undefined);
+// // 12 - empty values
+// console.log(typeof null, typeof undefined);
 
-console.log(null === undefined);
+// console.log(null === undefined);
 
-console.log(null == undefined);
+// console.log(null == undefined);
 
-console.log(null == false);
+// console.log(null == false);
 
-console.log(undefined == false);
+// console.log(undefined == false);
 
-// 13 - mudança de tipos
-console.log(5 * null);
+// // 13 - mudança de tipos
+// console.log(5 * null);
 
-console.log("teste" * "opa");
+// console.log("teste" * "opa");
 
-console.log("10" + 1);
+// console.log("10" + 1);
 
-console.log("10" - 1);
+// console.log("10" - 1);
